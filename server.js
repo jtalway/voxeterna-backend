1bbec80a1287a7fbdef5db665c588b7c74f7ebf3
Jason Alway <jtalway@gmail.com>
2020-05-21 11:35:42 -0500

    added parameterLimit

--- server.js
+++ server.js
@@ -41,9 +41,10 @@ app.use(express.static('public'));
 app.use(cors());
 app.use(morgan("dev"));
 // added to fix PayloadTooLargeError
-app.use(express.json({limit: "50mb"}));
-app.use(express.urlencoded({limit: "50mb",
-  extended: true
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb",
+  extended: true,
+  parameterLimit: 50000
 }));
 // app.use(bodyParser.json());
 app.use(cookieParser());
